def91f34ae02908b44bd94d60868bce0f6a32677
神保悟 <32728985+JinboSatoru@users.noreply.github.com>
2018-02-06 17:50:41 +0900

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,5 +5,3 @@ python makeHash.py >> /root/.jupyter/jupyter_notebook_config.py
 echo "c.notebookApp.open_browser = False" >> /root/.jupyter/jupyter_notebook_config.py
 
 echo "c.notebookApp.port = 18888" >> /root/.jupyter/jupyter_notebook_config.py
-
-jupyter notebook
