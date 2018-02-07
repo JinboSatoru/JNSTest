f9667bc23fe5c7455289e5f193a727fe95df975b
神保悟 <32728985+JinboSatoru@users.noreply.github.com>
2018-02-07 14:36:23 +0900

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 makeHash.py >> /root/.jupyter/jupyter_notebook_config.py
+python3 /JNSTest/makeHash.py >> /root/.jupyter/jupyter_notebook_config.py
 
 echo "c.notebookApp.open_browser = False" >> /root/.jupyter/jupyter_notebook_config.py
 
