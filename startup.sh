5a4f819272cccabcb1383428924dabed6ab8041c
神保悟 <32728985+JinboSatoru@users.noreply.github.com>
2018-02-07 14:56:17 +0900

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,9 +2,9 @@
 
 python3 /JNSTest/makeHash.py >> /root/.jupyter/jupyter_notebook_config.py
 
-echo "c.notebookApp.open_browser = False" >> /root/.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.open_browser = False" >> /root/.jupyter/jupyter_notebook_config.py
 
-echo "c.notebookApp.port = 8888" >> /root/.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.port = 8888" >> /root/.jupyter/jupyter_notebook_config.py
 
 cd /notebooks
 
